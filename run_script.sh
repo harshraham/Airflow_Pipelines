206c7e3ae0f43872292c08569a896272bc472e3e
harshraham <harshrahamatkar@gmail.com>
2024-09-15 21:07:16 +0530

    added runscript

--- run_script.sh
+++ run_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-export AIRFLOW_UID=50000
-export AIRFLOW_GID=0
+echo -e "AIRFLOW_UID=$(id -u)" > .env
 docker-compose up airflow-init
 docker-compose up
 docker exec -d piwebscraping-airflow-webserver-1 airflow dags unpause pipeline_1 pipeline_2
